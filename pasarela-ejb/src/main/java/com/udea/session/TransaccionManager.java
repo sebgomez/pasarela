2f15d31a27f0495648d7c8a5d790e3e873b09596
Sebastián <sebastian.gomez18@udea.edu.co>
2019-07-19 13:14:43 -0500

    Primer commit

---
+++ pasarela-ejb/src/main/java/com/udea/session/TransaccionManager.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.udea.session;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Sebastián
+ */
+@Stateless
+public class TransaccionManager implements TransaccionManagerLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
