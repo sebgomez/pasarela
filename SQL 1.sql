6cde3e5f7b321b66dc5ab1103ace2fd64ad169ff
Sebastián <sebastian.gomez18@udea.edu.co>
2019-07-27 12:35:06 -0500

    añadir script bases de datos

---
+++ SQL 1.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Transaccion(
+id INT NOT NULL,
+valor INT NOT NULL,
+tarjeta_credito INT REFERENCES CreditCard(id),
+cliente INT REFERENCES Customer(id),
+marca_tiempo TIMESTAMP, 
+PRIMARY KEY(id));
+CREATE TABLE CreditCard(
+id INT NOT NULL,
+owner INT REFERENCES Customer(id),
+cvv INT NOT NULL,
+fecha_vencimiento DATE,
+PRIMARY KEY(id));
+CREATE TABLE Customer(
+id INT NOT NULL,
+nombre VARCHAR(50) NOT NULL,
+apellido VARCHAR(50) NOT NULL,
+email VARCHAR(50) NOT NULL,
+PRIMARY KEY(id));
---
+++ pasarela-web/src/main/java/com/udea/controller/CustomerMBean.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.udea.controller;
+
+/**
+ *
+ * @author Sebastián
+ */
+public class CustomerMBean {
+
+    /**
+     * Creates a new instance of CustomerMBean
+     */
+    public CustomerMBean() {
+    }
+    
+}
